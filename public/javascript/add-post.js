160e53ae3f5674c4826169a6b2a487f7645c1322
Ryan Dungan <rd3@knights.ucf.edu>
2021-10-07 18:14:59 -0400

    fixed post bug, added CSS to login/register page

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -2,7 +2,7 @@ async function newFormHandler(event) {
     event.preventDefault();
 
     const title = document.querySelector('input[name="post-title"]').value;
-    const content = document.querySelector('input[name="content"]').value;
+    const content = document.querySelector('textarea[name="post-content"]').value;
 
     const response = await fetch('/api/posts', {
         method: 'POST',
@@ -21,3 +21,5 @@ async function newFormHandler(event) {
         alert(response.statusText);
     }
 }
+
+document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
