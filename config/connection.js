613ace93599e0916b7b8de14368d76aeb1d1956f
Ryan Dungan <rd3@knights.ucf.edu>
2021-10-08 13:28:11 -0400

    updated various files

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
-      port: process.env.PORT
+      port: 3306
     });
 
 
