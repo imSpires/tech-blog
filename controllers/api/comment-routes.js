e0d3ec1d5a26f8a5c6a33eeda3ba46da7de41533
Ryan Dungan <rd3@knights.ucf.edu>
2021-10-08 13:13:08 -0400

    updated connection.js

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -3,14 +3,14 @@ const { Comment } = require('../../models');
 const sequelize = require('../../config/connection');
 const withAuth = require('../../utils/auth');
 
-// router.get('/', (req, res) => {
-//     Comment.findAll({})
-//         .then(dbCommentData => res.json(dbCommentData))
-//         .catch(err => {
-//             console.log(err);
-//             res.status(500).json(err);
-//         })
-// });
+router.get('/', (req, res) => {
+    Comment.findAll({})
+        .then(dbCommentData => res.json(dbCommentData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+});
 
 router.get('/:id', (req, res) => {
     Comment.findAll({
